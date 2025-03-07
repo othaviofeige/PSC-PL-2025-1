fa37dadffe115cbfb129e3d9821606eaf2ba6436
othaviofeige <825139639@ulife.com.br>
2025-03-07 11:34:24 -0300

    criação dos Operadores na classe

--- Operadores.java
+++ Operadores.java
@@ -10,14 +10,34 @@ public class Operadores{
     System.out.println(a % b);
 
      // Operadores de comparação
-     int c = 20
-     int d = 15
+     int c = 20;
+     int d = 15;
      System.out.println(c == d);
      System.out.println(c != d);
      System.out.println(c > d);
      System.out.println(c < d);
      System.out.println(c >= d);
      System.out.println(c <= d);
+
+      // Operadores lógicos
+      boolean e = true;
+      boolean f = false;
+      System.out.println(e && f);
+      System.out.println(e || f);
+      System.out.println(!e);
+       
+       // Operadores de atribuição
+       int g = 10;
+
+       g+=5;  //g = g + 5 = 15
+       g-=5;  //g = g - 5 = 5
+       g/=5;  //g = g / 5 = 2
+       g*=5;  //g = g * 5 = 50
+       g%=5;  //g = g % 5 = 0
+       
+
+
+      
 
     
     }
